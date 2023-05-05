19cdd0fedc21a1420a3683dbf23e671a7c83f28c
NicolasFBorges <130315060+NicolasFBorges@users.noreply.github.com>
2023-05-05 16:00:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,78 @@
 const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
+let pipe = document.querySelector('.pipe');
+const scoreElement = document.querySelector('.score');
+const highscoreElement = document.querySelector('.highscore');
+let score = 0;
+let hasPassedPipe = false;
+let highscore = localStorage.getItem('highscore') || 0;
+highscoreElement.textContent = highscore;
+const retryButton = document.querySelector('.retry-button');
+let isGameOver = false;
+let isJumping = false;
 
-const jump = () => {
+const jump = (event) => {
+    if (event.key !== ' ' || isJumping) {
+        return;
+    }
+    isJumping = true;
     mario.classList.add('jump');
 
     setTimeout(() => {
         mario.classList.remove('jump');
+        isJumping = false;
     }, 500);
 }
 
-const loop = setInterval(() => {
+const increaseScore = () => {
+    score++;
+    scoreElement.textContent = score;
+    if (score > highscore) {
+        highscore = score;
+        highscoreElement.textContent = highscore;
+        localStorage.setItem('highscore', highscore);
+    }
+}
+
+const showRetryButton = () => {
+    retryButton.style.display = 'block';
+    retryButton.addEventListener('click', restartGame);
+}
+
+const restartGame = () => {
+    location.reload();
+};
+
+const loop = () => {
+    if (isGameOver) {
+        return;
+    }
 
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); 
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <= 140 && pipePosition >0 && marioPosition < 80) {
-      
-        pipe.style.animation = 'none';
+    if (pipePosition <= 140 && pipePosition > 0 && marioPosition < 80) {
+        pipe.classList.remove('pipe-animation');
         pipe.style.left = `${pipePosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
         mario.src = './images/super-mario-world-panicking.gif';
-        mario.style.width = '192px'
+        mario.style.width = '192px';
 
-        clearInterval(loop);
-        
+        showRetryButton();
+        isGameOver = true;
+    } else if (pipePosition <= 140 && pipePosition > 0 && marioPosition >= 80) {
+        if (!hasPassedPipe) {
+            increaseScore();
+            hasPassedPipe = true;
+        }
+    } else if (pipePosition <= 0) {
+        hasPassedPipe = false;
     }
-
-}, 10)
+    
+    requestAnimationFrame(loop);
+};
 
 document.addEventListener('keydown', jump);
-document.addEventListener('touchstart', jump);
+requestAnimationFrame(loop);
